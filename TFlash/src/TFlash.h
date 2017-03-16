eaebde9afd8ccd88658f22334b32f9b089c3d55c
たま吉さん(Tamakichi-San) <tamacat2014@gmail.com>
2017-03-16 21:10:34 +0900

    初版v1.0

---
+++ TFlash/src/TFlash.h
@@ -0,0 +1,38 @@
+//
+// File: TFlash.h
+// Arduino STM32 内部フラッシュメモリ書き込みライブラリ V1.0
+// 作成日 2017/03/16 by たま吉さん
+//
+
+#ifndef __TFLASH_H__
+#define __TFLASH_H__
+
+// FLASHメモリステータス
+typedef enum  {
+  TFLASH_BUSY = 1,
+  TFLASH_ERROR_PG,
+  TFLASH_ERROR_WRP,
+  TFLASH_ERROR_OPT,
+  TFLASH_COMPLETE,
+  TFLASH_TIMEOUT,
+  TFLASH_BAD_ADDRESS
+} TFLASH_Status;
+  
+class TFlash_Class {
+  public:
+    TFLASH_Status waitOperation(uint32_t tm);
+    TFLASH_Status eracePage(uint32_t pageAddress);
+    TFLASH_Status write(uint16_t* adr, uint16_t data);
+    TFLASH_Status write(uint16_t* adr, uint8_t* data, uint16_t len);
+    uint16_t read(uint16_t* adr) {return *((uint16_t*)adr);};
+    
+    void lock();
+    void unlock();
+
+  private:
+    TFLASH_Status getStatus(); 
+};
+
+extern TFlash_Class TFlash;
+
+#endif
